ceb0af0ca7d15cdac30964ec96c005135819c362
kirandev <kirandev@example.ru>
2020-10-12 07:14:48 +0500

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stepic_tours.wsgi
--- stepic_tours/settings.py
+++ stepic_tours/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'mob&9e%02!3_*p6zb+et9kzwl1hbhj16guf*-fx&rxk2*z(5&6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
